fdea2f0146161e05bffdf44734c0b8a36a507a23
jimmyLi <13420134410@163.com>
2025-10-03 13:41:06 +0800

    update new section

--- client/src/pages/landing.tsx
+++ client/src/pages/landing.tsx
@@ -2,6 +2,7 @@ import { Navigation } from "@/components/Navigation";
 import { HeroSection } from "@/components/HeroSection";
 import { FeaturesSection } from "@/components/FeaturesSection";
 import { HowItWorksSection } from "@/components/HowItWorksSection";
+import { ElaborationFeaturesSection } from "@/components/ElaborationFeaturesSection";
 import { WaitlistSection } from "@/components/WaitlistSection";
 import { Footer } from "@/components/Footer";
 
@@ -12,6 +13,7 @@ export default function Landing() {
       <HeroSection />
       <FeaturesSection />
       <HowItWorksSection />
+      <ElaborationFeaturesSection />
       <WaitlistSection />
       <Footer />
     </div>
